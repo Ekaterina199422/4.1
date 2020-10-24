9cce747954e267c430616d3db0480f37b82c4a7e
katerina kulchitskay <rfvb.foto@mail.ru>
2020-10-24 11:48:04 +0200

    first commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,48 +1,34 @@
+import kotlin.math.roundToInt
+
 fun main() {
-    val cardType = "MasterCard"
-    val monthTransfer = 320_000_000L
-    val transfer = 2_000_000L
-    commissionAmount(cardType, monthTransfer, transfer)
+    val payMaxDay = 150_000
+    val payMaxMounth = 600_000
+    val vkPayMaxDay = 15_000
+    val vkPayMaxMounth = 40_000
+
+    val payDay = 110_000
+    val vkPayDay = 11_000
+
+    val result = pay("VkPay", 30_000, 3_000, payMaxDay, payMaxMounth, vkPayMaxDay, vkPayMaxMounth, payDay, vkPayDay)
+    println(if (result != null) "Коммиссия составила $result" else "Превышен лимит!")
 }
 
-fun commissionAmount(
-    cardType: String = "VK Pay",
-    monthTransfer: Long = 0L,
-    transfer: Long
-): Long {
-    return when (cardType) {
-        "MasterCard", "Maestro" -> {
-            calForMasterMaestroCard(monthTransfer, transfer)
+fun pay(
+        card: String = "VkPay",
+        lastAmount: Int = 0,
+        thisAmount: Int,
+        payMaxDay: Int,
+        payMaxMounth: Int,
+        vkPayMaxDay: Int,
+        vkPayMaxMounth: Int,
+        payDay: Int,
+        vkPayDay: Int
+): Int? {
+    return if ((lastAmount + thisAmount) < payMaxMounth && (payDay + thisAmount) < payMaxDay) {
+        when (card) {
+            "Mastercard", "Maestro" -> if (lastAmount < 75_000) 0 else ((thisAmount / 100 * 0.6) + 2000).roundToInt()
+            "Visa", "МИР" -> if ((thisAmount / 100 * 75) < 3500) 3500 else (thisAmount / 100 * 0.75).roundToInt()
+            else -> if ((vkPayDay + thisAmount) < vkPayMaxDay && (lastAmount + thisAmount) < vkPayMaxMounth) 0 else null
         }
-        "Visa", "Мир" -> {
-            calForVisaMirCard(transfer)
-        }
-        "VK Pay" -> {
-            println("Комиссия не взимается")
-            0
-        }
-        else -> 0
-    }
-}
-
-private fun calForVisaMirCard(transfer: Long): Long {
-    val commission = transfer * 0.0075
-    return if (commission > 3500) {
-        println("Комиссия: $commission коп")
-        commission.toLong()
-    } else {
-        println("Комиссия: 3500 коп")
-        3500
-    }
-}
-
-private fun calForMasterMaestroCard(monthTransfer: Long, transfer: Long): Long {
-    return if (monthTransfer + transfer <= 7_500_000) {
-        println("Комиссия не взимается")
-        0
-    } else {
-        val commission = transfer * 0.006 + 2000
-        println("Комиссия: $commission коп")
-        commission.toLong()
-    }
+    } else null
 }
